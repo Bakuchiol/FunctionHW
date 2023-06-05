eadeb144bb7fd214aed9faed4ed00b4a88ecec08
Lynette <lynettecargo@Lynettes-Air.home>
2023-06-05 17:25:45 -0500

    problems 3-5

--- app.js
+++ app.js
@@ -26,17 +26,41 @@ const maxOfThree = (x, y, z) =>{
         return z;
     }
 } 
-console.log(maxOfThree(1,2,3));
+console.log(maxOfThree(1, 2, 3));   // 3
 
 
 // 3. Define a function, as a function declaration, isCharAVowel
 //  that takes a character as an argument and returns true if it is a vowel, false otherwise.
 
+const isCharAVowel = (char) => {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    return vowels.includes(char);
+}
+console.log(isCharAVowel('b'));     // false
+
 // 4. Define a function, as a function expression, sumArray that takes an array of numbers and returns the sum of those numbers.
 //  For example, sumArray([2, 4, 5]);would return 11.
 
+const sumArray = (arr) => {
+    let sumArray = 0;
+    for (let i = 0; i < arr.length; i++){
+        sumArray += arr[i]
+    }
+    return sumArray
+}
+console.log(sumArray([1, 2, 3]));   //6
+
 // 5. Define a function, as a function declaration, multiplyArray that takes an array of numbers and returns the product those numbers.
 //  For example, multiplyArray([2, 4, 5]);would return 40.
+
+const multiplyArray = (arr) => {
+    let multiplyArray = 1;
+    for (let i = 0; i < arr.length; i++){
+        multiplyArray *= arr[i]
+    }
+    return multiplyArray
+}
+console.log(multiplyArray([2, 3, 5]));      // 30
 
 // 6. Define a function, as a function expression, numArgs that returns the number of arguments passed to the function when called.
 
